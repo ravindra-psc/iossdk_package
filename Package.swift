be353b8766b1fde3b2d3000020e974789e319b5a
Ravindra Chouhan <ravindrapal.chouhan@nuance.com>
2022-12-09 03:51:42 +0530

    Use version 0.0.3 zip

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     targets: [
         .binaryTarget(
                     name: "DragonMedicalSpeechKit",
-                    url: "https://github.com/ravindra-psc/iossdk_xcframework/blob/main/DragonMedicalSpeechKit.xcframework-0.0.1.zip",
+                    url: "https://github.com/ravindra-psc/iossdk_xcframework/blob/main/0.0.3.zip",
                     checksum: "6997b047c6e2d69cd12caa4b2da75e428e0cbd3ae111033c25b36c81615677ff"
                 ),
     ]
